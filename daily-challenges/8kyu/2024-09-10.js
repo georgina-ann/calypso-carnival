402d4cca4d904ad1eb2c32c4a1502b2848eecfe7
Georgina Ann <hi.georgina.ann@gmail.com>
2024-09-10 19:56:02 -0400

    added daily challenge

---
+++ daily-challenges/8kyu/2024-09-10.js
@@ -0,0 +1,10 @@
+//find the needle in the haystack!
+
+function findNeedle(haystack) {
+    // your code here
+    let index = haystack.indexOf('needle')
+    
+    if (haystack.includes('needle')){
+        return (`found the needle at position ${index}`)
+      }
+    }
