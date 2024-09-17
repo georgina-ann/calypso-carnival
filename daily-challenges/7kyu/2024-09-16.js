59c9a1a454d53fe9e0fa071d4cc08b2dc30933ef
Georgina Ann <hi.georgina.ann@gmail.com>
2024-09-16 20:44:23 -0400

    added coding challenge

---
+++ daily-challenges/7kyu/2024-09-16.js
@@ -0,0 +1,8 @@
+//find the sum of the smallest numbers in array
+
+function sumTwoSmallestNumbers(numbers,int) {  
+    //Code here
+    numbers.sort(function (a,b){return a - b})
+        return (numbers[0] + numbers[1])
+      }
+  
