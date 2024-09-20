17309c9cbce9f4a6d2f3848194a1f8f2e63c99c0
Georgina Ann <hi.georgina.ann@gmail.com>
2024-09-19 20:31:17 -0400

    added daily challenge

---
+++ daily-challenges/8kyu/2024-09-19.js
@@ -0,0 +1,10 @@
+//parse integer from a string
+
+function getAge(inputString){
+    // return the girl's correct age as an integer. Happy coding :) 
+      //take in a string, the first index is always a number
+      let integer = inputString[0]
+      //return that first index as a number()
+      return Number(integer)
+      
+    }
