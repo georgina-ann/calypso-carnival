94578ecccc0404a0da57a04212f5fece7b239aee
Georgina Ann <hi.georgina.ann@gmail.com>
2024-09-20 20:32:28 -0400

    added coding challenge

---
+++ daily-challenges/8kyu/2024-09-20.js
@@ -0,0 +1,7 @@
+//return sum of values given
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    // code goes here
+    let totalGoals = laLigaGoals += copaDelReyGoals += championsLeagueGoals;
+    return totalGoals;
+  }
