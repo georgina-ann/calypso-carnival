e082741e7240cc4c61d9592d7ea7718b0a9bbfee
Georgina Ann <hi.georgina.ann@gmail.com>
2024-09-27 20:41:55 -0400

    added daily challenge

---
+++ daily-challenges/8kyu/2024-09-30.js
@@ -0,0 +1,10 @@
+//return the sum of elements in array
+
+var summation = function (num) {
+    // Code here
+    let result = 0
+    for(let i = 0; i <= num; i++){
+    result += i
+      }
+    return result
+  }
